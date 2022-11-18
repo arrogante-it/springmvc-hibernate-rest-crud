33bbb4a474b17af49df74a4db6de8dc6bb32fc7a
Alexandr <alexandrsinko2000@gmail.com>
2022-11-18 18:15:57 +0200

    add more functionality

--- src/main/java/com/alexandrsinko/spring/rest/controller/RESTController.java
+++ src/main/java/com/alexandrsinko/spring/rest/controller/RESTController.java
@@ -1,11 +1,13 @@
 package com.alexandrsinko.spring.rest.controller;
 
 import com.alexandrsinko.spring.rest.entity.Employee;
+import com.alexandrsinko.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.alexandrsinko.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.alexandrsinko.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +26,43 @@ public class RESTController {
     public List<Employee> getAllEmployees() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
+    }
+
+    @GetMapping(value = "/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in DataBase");
+        }
+
+        return employee;
+    }
+
+    @PostMapping(value = "/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping(value = "/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping(value = "/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = "
+                    + id + " in DataBase");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
     }
 }
 
